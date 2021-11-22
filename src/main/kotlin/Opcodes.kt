76a139322376dad54526d45d004d85bfea8c6d59
klayton smith <motoklay@gmail.com>
2021-11-22 18:52:40 -0500

    added opcodes

--- src/main/kotlin/Opcodes.kt
+++ src/main/kotlin/Opcodes.kt
@@ -1,2 +1,30 @@
 enum class Opcodes {
+    //2 arg
+    ADD(),
+    SUB(),
+    MUL(),
+    DIV(),
+    SHL(),
+    SHR(),
+    AND(),
+    OR(),
+    XOR(),
+    MOV(),
+    SAV(),
+    //1 arg
+    LD(),
+    NOT(),
+    INC(),
+    DEC(),
+    JMP(),
+    JLT(),
+    JMT(),
+    JEQ(),
+    //no arg
+    TRU(),
+    FAL(),
+    PSH(),
+    POP(),
+    IMM(),
+    JIM()
 }
